f89e9ad5e0b631f75d92e57614d57ca5bc5e57a5
Angitha S Anil <angithasanil19@gmail.com>
2025-10-16 09:52:58 +0530

    day2 hw

---
+++ welcome/src/main/java/com/example/welcom/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.welcom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/start")
+    public String redirectToWelcome() {
+        return "redirect:/welcome";
+    }
+
+    @GetMapping("/welcome")
+    public String welcomePage() {
+        return "welcome";
+    }
+}
